7e92cb9418501cb5d618d364bb1be495a87914be
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-11-23 16:57:31 -0700

    added all the .get functions

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 const path = require("path");
+const noteData = require("./db/db.json");
 
 app.use(express.static("public"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => res.json(noteData));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.listen(PORT, () =>
