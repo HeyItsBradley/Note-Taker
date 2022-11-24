1fd8192cffa8d9b9d9527aa7aeafa3cada4fd40f
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-11-23 19:18:07 -0700

    added post functino

--- server.js
+++ server.js
@@ -1,16 +1,62 @@
+const exp = require("constants");
+const { randomUUID } = require("crypto");
+const { response } = require("express");
 const express = require("express");
+const { fstat } = require("fs");
+const { request } = require("http");
 const app = express();
 const PORT = 3001;
 const path = require("path");
 const noteData = require("./db/db.json");
+const fs = require("fs");
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
 
 app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("/api/notes", (req, res) => res.json(noteData));
+
+app.post("/api/notes", (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newEntry = {
+      title,
+      text,
+      entry_id: randomUUID(),
+    };
+
+    const entryString = JSON.stringify(newEntry);
+
+    readAndAppend(newEntry, "./db/db.json");
+
+    // fs.appendFile("./db/db.json", entryString, (err) =>
+    //   err ? console.error(err) : console.log("entry was appended!")
+    // );
+  }
+});
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
